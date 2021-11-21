089684efb878a5db2c9cfa3c23b85854ec4f28da
Nirad <yeola.nirad@gmail.com>
2021-11-21 17:51:31 +0530

    started with request match screen

--- root/frontend/sportistaan/lib/screens/home.dart
+++ root/frontend/sportistaan/lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sportistaan/constants.dart';
 
 class Home extends StatelessWidget {
   const Home({ Key? key }) : super(key: key);
@@ -6,20 +7,33 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // drawer: ,
       appBar: AppBar(
-        leading:const CircleAvatar(
-          radius: 40,
-          backgroundColor: Colors.grey,
-        ),
+        backgroundColor: const Color(0xFF24243E),
+        leading: Builder(
+              builder: (context) => IconButton(
+                icon: const CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.grey,
+                ), 
+                onPressed: () => Scaffold.of(context).openDrawer(),
+              ),
+            ),
+        
         
       ),
-      body: Center(child: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: <Color>[Color(0xff0F0C29),Color(0xff302B63),Color(0xff24243E)]
-          )),)),
+      body: Container(
+        decoration: kBoxDecoration,
+        child: const Center(
+          child: Text(
+            'Sportistaan',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+            ),
+        ),
+            ),
     );
   }
 }
--- root/frontend/sportistaan/lib/screens/request.dart
+++ root/frontend/sportistaan/lib/screens/request.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../constants.dart';
+
 class RequestPage extends StatefulWidget {
   const RequestPage({ Key? key }) : super(key: key);
 
@@ -10,8 +12,57 @@ class RequestPage extends StatefulWidget {
 class _RequestPageState extends State<RequestPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Request a match'),
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Request a match',
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w700,
+          ),),
+        backgroundColor: const Color(0xFF24243E),
+        leading: Builder(
+              builder: (context) => IconButton(
+                icon: const CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.grey,
+                ), 
+                onPressed: () => Scaffold.of(context).openDrawer(),
+              ),
+            ),
+        
+        
+      ),
+      backgroundColor: const Color(0xff302B63),
+      extendBody: true,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        
+        children: <Widget>[
+          const Text(
+          'Genral Information',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.w700,
+          ),),
+          Container(
+            decoration: kBoxDecoration,
+            child: Column(
+              children: [
+                const Text('Event Name:'),
+                 TextField(
+                  onChanged: (value) {
+                    print(value);
+                  },
+                  maxLength: 50,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+),
+                ),
+              ],),
+          ),
+        ],
+            ),
     );
   }
 }
