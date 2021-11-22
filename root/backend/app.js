ca716b4386ae05f6099a3d586cdc9e09cd330237
archit jain <arcyjain2002@gmail.com>
2021-11-22 23:56:27 +0530

    res added for post request

--- root/backend/app.js
+++ root/backend/app.js
@@ -74,8 +74,8 @@ app.post("/join-event", (req, res) => {
     { _id: req.body._id },
     { $push: { student_info: { name: req.body.name } } } //  {_id: "12345", student_id: person's Name}
   )
-    .then((result) => console.log(result))
-    .catch((err) => console.log(err));
+    .then((result) => res.status(200))
+    .catch((err) => res.status(400));
 });
 
 // route to all the information of the scheduled events.
