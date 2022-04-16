754b44affd11469e10cc0e038f577c41a6180bee
Archit Jain <arcyjain2002@gmail.com>
2022-04-16 12:02:01 +0530

    checking for empty array

--- root/backend/app.js
+++ root/backend/app.js
@@ -72,7 +72,7 @@ app.post("/create-event", (req, res) => {
 app.post("/join-event", (req, res) => {
   Event.find({ student_info: { $elemMatch: { name: req.body.name } } })
     .then((result) => {
-      if (result == null) {
+      if (result.length == 0) {
         Event.updateMany(
           { _id: req.body._id },
           { $push: { student_info: { name: req.body.name } } }
